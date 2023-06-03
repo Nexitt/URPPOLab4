9d404b41701868d22ff656b0dbe7651759032890
Nexitt <41379061+Nexitt@users.noreply.github.com>
2023-06-03 20:55:51 +0700

    Update calculator.py
    Обновлено по спецификации pep8

--- calculator.py
+++ calculator.py
@@ -1,18 +1,22 @@
 # функция для сложения двух чисел
 def add(x, y):
-   return x + y
+    return x + y
+
 
 # функция для вычитания двух чисел
 def subtract(x, y):
-   return x - y
+    return x - y
+
 
 # функция для умножения двух чисел
 def multiply(x, y):
-   return x * y
+    return x * y
+
 
 # функция для деления двух чисел
 def divide(x, y):
-   return x / y
+    return x / y
+
 
 # вывод на экран доступных операций
 print("Выберите операцию.")
@@ -30,16 +34,16 @@ num2 = float(input("Введите второе число: "))
 
 # выполнение выбранной операции и вывод результата
 if choice == '1':
-   print(num1,"+",num2,"=", add(num1,num2))
+    print(num1, "+", num2, "=", add(num1, num2))
 
 elif choice == '2':
-   print(num1,"-",num2,"=", subtract(num1,num2))
+    print(num1, "-", num2, "=", subtract(num1, num2))
 
 elif choice == '3':
-   print(num1,"*",num2,"=", multiply(num1,num2))
+    print(num1, "*", num2, "=", multiply(num1, num2))
 
 elif choice == '4':
-   print(num1,"/",num2,"=", divide(num1,num2))
+    print(num1, "/", num2, "=", divide(num1, num2))
 
 else:
-   print("Некорректный выбор операции")
+    print("Некорректный выбор операции")
